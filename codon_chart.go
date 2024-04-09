176802a5c6f614ef2270a03a2ac175f03e5c2648
Ashna Guha <57196878+TheLabradorScientist@users.noreply.github.com>
2024-04-09 16:18:26 -0400

    commit updated base transcription

--- codon_chart.go
+++ codon_chart.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aaaf750aedcdbcd15d54767f25058ac9304b7c4db52cab7565c421c84e964001
-size 2521
+oid sha256:d216fad7817e13d7af2f91bec17ccc025fe07cd7c9c9f9d96c626df9ba487124
+size 2478
