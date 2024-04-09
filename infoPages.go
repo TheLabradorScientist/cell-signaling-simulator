fcd6f569c4d16dc4e6abfdaf8f7608a71a943f46
Ashna Guha <57196878+TheLabradorScientist@users.noreply.github.com>
2024-04-09 16:18:26 -0400

    commit updated base transcription

--- infoPages.go
+++ infoPages.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15eba79d99383e992a3fd18c2d61a1f6e2b8ac26f0478d5283fba78b93dcf1dd
-size 1523
+oid sha256:9b39801899df546a7954da8dc3b8edb62910263624ffa5062bbd19b7dc855e3d
+size 1624
